f7208efe09947c1b8e674328718511e2dd0191bf
David Zemler <70216684+dlz215@users.noreply.github.com>
2022-07-01 12:57:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// REPLACE ALL
+const str1 = "vothana is the best of the best"
+console.log(str1.replaceAll('best', 'worst'));
+
+// REPLACE
+// older method, only replaces first instance
+const str2 = "abababab"
+console.log(str2.replaceAll('b', 'c'));
+console.log(str2.replace('b', 'c'));
